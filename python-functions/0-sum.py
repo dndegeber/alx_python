162a1febf5444c8b260ebbd4d75ceae9e56b4b7e
dndegeber <dorandege@gmail.com>
2023-08-23 16:31:52 +0300

    Sum of integer

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,8 +1,7 @@
-
 def add(a, b):
-    return a + b 
-    result =add (a, b)
-    add(1,2)
-    add(100,-2)
-    add(-100,-2)
-    add(0,0)
+    return a+b
+    
+add(1,2)
+add(100,-2)
+add(-100,-2)
+add(0,0)
