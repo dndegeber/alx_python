7ab559f8f55798577e1ca28139d15a0665242574
dndegeber <dorandege@gmail.com>
2023-08-24 01:07:44 +0300

    Reverse string

---
+++ python-functions/3-string.py
@@ -0,0 +1,8 @@
+def reverse_string(string):
+    return string[::-1]
+    input_string = "hello"
+reversed_string = reverse_string(input_string)
+reverse_string("Hello")
+reverse_string("")
+reverse_string("madam")
+reverse_string("Hello, World!")
