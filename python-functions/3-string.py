eeb7155a65164a9482ae811ad7a27a748a3464b4
dndegeber <dorandege@gmail.com>
2023-08-24 01:11:22 +0300

    Reverse string

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,7 +1,7 @@
 def reverse_string(string):
     return string[::-1]
     input_string = "hello"
-reversed_string = reverse_string(input_string)
+reversed_string = reverse_string
 reverse_string("Hello")
 reverse_string("")
 reverse_string("madam")
