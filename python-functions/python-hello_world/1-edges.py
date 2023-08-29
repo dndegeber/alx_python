50f60f2d76f9ea12f70a21c83416e4c56d451d4f
dndegeber <dorandege@gmail.com>
2023-08-29 11:31:04 +0300

    1-edges.py

---
+++ python-functions/python-hello_world/1-edges.py
@@ -0,0 +1,6 @@
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+"First 3 letters: {}".format(word_first_3)
+"Last 2 letters: {}".format(word_last_2)
+"Middle word: {}".format(middle_word)
