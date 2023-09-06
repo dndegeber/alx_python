893627f4628ff8db619a2297f6cd6adcbfe86397
dndegeber <dorandege@gmail.com>
2023-09-06 17:09:04 +0300

    Square size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,16 +1,35 @@
-Square = __import__('0-square').Square
+#!/usr/bin/python3
 
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
+class Square:
+    """
+    This class defines a square.
 
-try:
-    print(my_square.size)  # This should raise an error
-except Exception as e:
-    print(e)
+    Attributes:
+        __size (int): The size of the square.
+    """
 
-try:
-    print(my_square.__size)  # This should raise an error
-except Exception as e:
-    print(e)
+    def __init__(self, size):
+        """
+        Initializes a new Square object.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
+
+if __name__ == "__main__":
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+
+    try:
+        print(my_square.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square.__size)
+    except Exception as e:
+        print(e)
+
 
