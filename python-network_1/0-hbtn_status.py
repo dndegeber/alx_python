2d2894bb338c4d31fdf433353ca978495169cacc
dndegeber <dorandege@gmail.com>
2023-09-20 13:39:00 +0300

    What's my  status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,18 @@
 import requests
 
 def fetch_status():
-    url = "https://intranet.hbtn.io/status"
+    """
+    Fetches the status from https://alu-intranet.hbtn.io/status using requests module.
+    Displays the response in a specific format.
+    """
+    url = "https://alu-intranet.hbtn.io/status"
     response = requests.get(url)
     content_type = type(response.text).__name__
     content = response.text
     
     print("Body response:")
-    print(f"    - type: {content_type}")
-    print(f"    - content: {content}")
+    print("    - type: {}".format(content_type))
+    print("    - content: {}".format(content))
 
 if __name__ == "__main__":
     fetch_status()
