46d1ed12f8c9f978bced843ad6bd066a1a1f76b8
dndegeber <dorandege@gmail.com>
2023-09-20 13:25:36 +0300

    What's my status

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+import requests
+
+def fetch_status():
+    url = "https://alu-intranet.hbtn.io/status"
+    response = requests.get(url)
+    content_type = type(response.text).__name__
+    content = response.text
+    
+    print("Body response:")
+    print(f"    - type: {content_type}")
+    print(f"    - content: {content}")
+
+if __name__ == "__main__":
+    fetch_status()
