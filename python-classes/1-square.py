eceb4376eb360e2a90cbae403e2967fc8298ddde
dndegeber <dorandege@gmail.com>
2023-10-05 13:46:09 +0300

    Size validation correction

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python3
+"""
+This module defines the Square class, which represents a square.
+
+Classes:
+    Square: A class representing a square.
+"""
+
 
 class Square:
     """
-    This class defines a square.
+    A class representing a square.
 
     Attributes:
         __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size=0): Initializes a Square instance with an optional size.
     """
 
     def __init__(self, size=0):
         """
-        Initializes a new Square object with an optional size.
+        Initializes a Square instance with an optional size.
 
         Args:
-            size (int, optional): The size of the square (default is 0).
-        
+            size (int, optional): The size of the square. Defaults to 0.
+
         Raises:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
@@ -24,36 +34,3 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
-if __name__ == "__main__":
-    my_square_1 = Square(3)
-    print(type(my_square_1))
-    print(my_square_1.__dict__)
-
-    my_square_2 = Square()
-    print(type(my_square_2))
-    print(my_square_2.__dict__)
-
-    try:
-        print(my_square_1.size)
-    except Exception as e:
-        print(e)
-
-    try:
-        print(my_square_1.__size)
-    except Exception as e:
-        print(e)
-
-    try:
-        my_square_3 = Square("3")
-        print(type(my_square_3))
-        print(my_square_3.__dict__)
-    except Exception as e:
-        print(e)
-
-    try:
-        my_square_4 = Square(-89)
-        print(type(my_square_4))
-        print(my_square_4.__dict__)
-    except Exception as e:
-        print(e)
