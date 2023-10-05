2ea8d521cdf4664a0c60e5b46fec4770e72f25ef
dndegeber <dorandege@gmail.com>
2023-10-05 15:12:53 +0300

    Base geometry

---
+++ python-inheritance/3-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Contains an empty class definition for BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class representing the base geometry.
+    """
+    def __init_subclass__(cls):
+        pass
+
+    def __dir__(self):
+        return [attr for attr in dir(type(self)) if attr != '__init_subclass__']
+
+    def __class__(self):
+        return type(self)
