abfa35a99b32509a018cee922e74a6541ebd6814
dndegeber <dorandege@gmail.com>
2023-10-11 12:48:36 +0300

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,8 +1,19 @@
 
+
 import requests
 import sys
 
+
 def get_employee_info(employee_id):
+    """
+    Fetches and prints information about a user's completed tasks.
+
+    Args:
+        employee_id (int): The ID of the employee.
+
+    Returns:
+        None
+    """
     user_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
     todo_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
 
@@ -10,13 +21,14 @@ def get_employee_info(employee_id):
         user_data = user_response.json()
         todo_data = todo_response.json()
 
-        completed_tasks = [task for task in todo_data if task['completed']]
-        
-        print(f"Employee {user_data['name']} is done with tasks({len(completed_tasks)}/{len(todo_data)}):")
+        completed_tasks = [task for task in todo_data if task.get('completed')]
+
+        print(f"Employee {user_data.get('name')} is done with tasks({len(completed_tasks)}/{len(todo_data)}):")
         for task in completed_tasks:
-            print(f"\t{task['title']}")
+            print(f"\t{task.get('title')}")
     else:
         print(f"Error fetching data for employee ID {employee_id}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -24,6 +36,5 @@ if __name__ == "__main__":
         sys.exit(1)
     employee_id = int(sys.argv[1])
     get_employee_info(employee_id)
-
 
 
