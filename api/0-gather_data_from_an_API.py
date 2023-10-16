3861349120ff388ffed68e11a29192a86e0e95f0
dndegeber <dorandege@gmail.com>
2023-10-16 13:03:38 +0300

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,34 +1,43 @@
+#!/usr/bin/python3
+"""
+Fetches employee info from REST API
+"""
+
 import requests
 import sys
 
+users_url = "https://jsonplaceholder.typicode.com/users"
+todos_url = "https://jsonplaceholder.typicode.com/todos"
+
+
 def get_employee_info(employee_id):
-    # Get employee details
-    user_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
-    user_data = user_response.json()
+    """ Fetch employee name, number of tasks """
+
+    user_data = requests.get(f"{users_url}/{employee_id}").json()
     employee_name = user_data['name']
 
-    # Get employee's TODO list
-    todo_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
-    todo_data = todo_response.json()
-
-    # Calculate completed tasks
-    completed_tasks = [task for task in todo_data if task['completed']]
-    num_completed_tasks = len(completed_tasks)
+    todo_data = requests.get(f"{users_url}/{employee_id}/todos").json()
+    num_completed_tasks = len([task for task in todo_data if task['completed']])
     total_tasks = len(todo_data)
 
-    # Display information
     print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):")
-    for task in completed_tasks:
+    for task in [task for task in todo_data if task['completed']]:
         print(f"\t{task['title']}")
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: python3 script_name.py <employee_id>")
-        sys.exit(1)
 
-    employee_id = int(sys.argv[1])
-    get_employee_info(employee_id)
-    
+if __name__ == "__main__":
+    def check_tasks(id):
+        """ Check student output for formatting """
 
+        filename = 'student_output'
+        count = 0
+        with open(filename, 'r') as f:
+            next(f)
+            for line in f:
+                count += 1
+                if line[0] == '\t' and line[1] == ' ' and line[-1] == '\n':
+                    print("Task {} Formatting: OK".format(count))
+                else:
+                    print("Task {} Formatting: Incorrect".format(count))
 
-
+    check_tasks(int(sys.argv[1]))
