59637a483490b1b9bffdb3c9cb31fb2d8fb25f16
dndegeber <dorandege@gmail.com>
2023-10-27 21:07:56 +0300

    All cities by state correction

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,55 +1,23 @@
-import MySQLdb
-import sys
+#!/usr/bin/python3
+"""takes in the name of a state as an argument and
+lists all cities of that state,
+using the database hbtn_0e_4_usa"""
 
-def filter_cities_by_state(username, password, database, state_name):
-    # Connect to MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        user=username,
-        passwd=password,
-        db=database,
-        port=3306
-    )
+if __name__ == '__main__':
 
-    # Create a cursor object
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
     cur = db.cursor()
-
-    # Use a parameterized query to prevent SQL injection
-    query = """
-    SELECT GROUP_CONCAT(name SEPARATOR ', ')
-    FROM cities
-    JOIN states ON cities.state_id = states.id
-    WHERE states.name = %s
-    ORDER BY cities.id
-    """
-    
-    # Execute the query with the user input as a parameter
-    cur.execute(query, (state_name,))
-
-    # Fetch the result
-    result = cur.fetchone()[0]
-
-    if result:
-        print(result)
-    else:
-        print("No cities found for the given state.")
-
-    # Close the cursor and connection
+    cur.execute("SELECT cities.name\
+                FROM cities LEFT JOIN states\
+                ON states.id = cities.state_id\
+                WHERE states.name = %s\
+                ORDER BY cities.id ASC", (sys.argv[4],))
+    rows = cur.fetchall()
+    print(", ".join([row[0] for row in rows]))
     cur.close()
     db.close()
-
-if __name__ == "__main__":
-    # Check if the correct number of arguments are provided
-    if len(sys.argv) != 5:
-        print("Usage: ./5-filter_cities.py <username> <password> <database> <state_name>")
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Call the function with provided arguments
-    filter_cities_by_state(username, password, database, state_name)
-
-
