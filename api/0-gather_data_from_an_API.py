75fde91a6f9b91ffd31ab8f9a9c31b10e165e686
dndegeber <dorandege@gmail.com>
2023-10-27 13:16:35 +0300

    Correction Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,38 +1,49 @@
-#!/usr/bin/python3
-
+#!/usr/bin/python
+"""
+Import requests module
+"""
 import requests
+"""
+Import sys module to get command line arguments
+"""
 import sys
 
+"""
+Get the employee ID from the first argument
+"""
+employee_id = sys.argv[1]
 
-def get_employee_info(employee_id):
-    """
-    Fetches and prints information about a user's completed tasks.
+"""
+Define the base URL for the API
+"""
+base_url = "https://jsonplaceholder.typicode.com/users/"
 
-    Args:
-        employee_id (int): The ID of the employee.
+"""Get the employee details from the API"""
+employee = requests.get(base_url + employee_id).json()
 
-    Returns:
-        None
-    """
-    user_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
-    todo_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
+"""Get the employee name"""
+employee_name = employee["name"]
 
-    if user_response.status_code == 200 and todo_response.status_code == 200:
-        user_data = user_response.json()
-        todo_data = todo_response.json()
+""" Get the employee TODO list from the API"""
+todos = requests.get(base_url + employee_id + "/todos").json()
 
-        completed_tasks = [task for task in todo_data if task.get('completed')]
+"""Initialize the total and done tasks counters"""
+total_tasks = 0
+done_tasks = 0
 
-        print(f"Employee {user_data.get('name')} is done with tasks({len(completed_tasks)}/{len(todo_data)}):")
-        for task in completed_tasks:
-            print(f"\t{task.get('title')}")
-    else:
-        print(f"Error fetching data for employee ID {employee_id}")
+"""Initialize an empty list for the done tasks titles"""
+done_tasks_titles = []
 
+"""Loop through the todos list and update the counters and titles list"""
+for todo in todos:
+    total_tasks += 1
+    if todo["completed"]:
+        done_tasks += 1
+        done_tasks_titles.append(todo["title"])
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py <employee_id>")
-        sys.exit(1)
-    employee_id = int(sys.argv[1])
-    get_employee_info(employee_id)
+"""Print the first line with the employee name and tasks progress"""
+print("Employee {} is done with tasks({}/{}):".format(employee_name, done_tasks, total_tasks))
+
+"""Print the titles of the done tasks with a tabulation and a space before each one"""
+for title in done_tasks_titles:
+    print("\t " + title)
