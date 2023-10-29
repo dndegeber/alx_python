df7077ed94d0b86a96fc4cb1a7970ec636446471
dndegeber <dorandege@gmail.com>
2023-10-29 17:40:21 +0300

    Export CSV correction

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,46 +1,18 @@
 #!/usr/bin/python3
-"""
-Fetches and exports information about a user's completed tasks in JSON format.
-"""
-
-import json
+"""Exports to-do list to CSV format"""
+import csv
 import requests
 import sys
 
-
-def get_employee_info(employee_id):
-    """
-    Fetches and exports information about a user's completed tasks in JSON format.
-
-    Args:
-        employee_id (int): The ID of the employee.
-
-    Returns:
-        None
-    """
-    user_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
-    todo_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
-
-    if user_response.status_code == 200 and todo_response.status_code == 200:
-        user_data = user_response.json()
-        todo_data = todo_response.json()
-
-        completed_tasks = [{"task": task['title'], "completed": task['completed'], "username": user_data['username']} for task in todo_data]
-
-        # Save to JSON file
-        file_name = f"{user_data['id']}.json"
-        with open(file_name, mode='w') as file:
-            json.dump({user_data['id']: completed_tasks}, file)
-
-        print(f"Data exported to {file_name}")
-    else:
-        print(f"Error fetching data for employee ID {employee_id}")
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py <employee_id>")
-        sys.exit(1)
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    employee_id = int(sys.argv[1])
-    get_employee_info(employee_id)
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
