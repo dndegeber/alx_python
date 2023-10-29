253b88d002ee26f6ba882162c61b1b49e0380643
dndegeber <dorandege@gmail.com>
2023-10-29 17:56:52 +0300

    Export recheck

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""Exports to-do list to CSV format"""
-import csv
+"""Exports to-do to JSON format with the whole dictionary"""
+import json
 import requests
-import sys
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    users = requests.get(url + "users").json()
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-         ) for t in todos]
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
